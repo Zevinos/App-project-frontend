9d26246712f2b0958eb09072207c9a7541af0a46
evanc <evan.coindreau@gmail.com>
2024-01-22 12:25:24 +0100

    set up done without api

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,18 @@
 import { useState } from "react";
 import "./App.css";
+import { Routes, Route } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import HomePage from "./pages/Homepage";
 
 function App() {
-  const [count, setCount] = useState(0);
-
-  return <div></div>;
+  return (
+    <div className="App">
+      <Navbar></Navbar>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+      </Routes>
+    </div>
+  );
 }
 
 export default App;
